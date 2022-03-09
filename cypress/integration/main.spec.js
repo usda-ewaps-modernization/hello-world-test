f2d4190ddf36b6affab87dcb5b7479589df16247
Adam Norris <anorris@rivasolutionsinc.com>
2022-03-09 15:48:24 -0500

    Another attempt

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -5,7 +5,7 @@ describe ('Base test of landing page', () => {
   it('Tests base page hello world output', () => {
     
     cy.visit('http://hello-world-app.dev.ewapsmod.demoriva.com/')
-    cy.get('[div] > [id="thing"]')
+    cy.get('body')
       .should('contain', 'hello -- this is from the hello world application!!!')
   })
 })
