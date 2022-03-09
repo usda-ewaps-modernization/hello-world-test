c6df95fc30fc7bcb8556318a1d48163554b9cf6d
Adam Norris <anorris@rivasolutionsinc.com>
2022-03-09 15:10:39 -0500

    Initial test creation

---
+++ cypress/integration/main.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe ('Base test of landing page', () => {
+
+  it('Base page test', () => {
+    
+    cy.visit('http://hello-world-app.dev.ewapsmod.demoriva.com/')
+    cy.get('<body>')
+      .should('have text', 'hello -- this is from the hello world application!!!')
+  })
+})
