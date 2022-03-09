52a2efb5efa1b9c7979e3f3a88c05f14cbb61a29
Adam Norris <anorris@rivasolutionsinc.com>
2022-03-09 15:42:57 -0500

    Updated test

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -6,6 +6,7 @@ describe ('Base test of landing page', () => {
     
     cy.visit('http://hello-world-app.dev.ewapsmod.demoriva.com/')
     cy.get('<body>')
+      .find('[id="thing"]')
       .should('have text', 'hello -- this is from the hello world application!!!')
   })
 })
